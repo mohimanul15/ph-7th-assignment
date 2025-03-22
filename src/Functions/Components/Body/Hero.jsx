d662479af2bdf0f8ed9325e0ffa34c45a852221d
mohimanul15 <mohimanul15@gmail.com>
2025-03-23 04:04:22 +0600

    Hero section complete

--- src/Functions/Components/Body/Hero.jsx
+++ src/Functions/Components/Body/Hero.jsx
@@ -1,8 +1,43 @@
+import Bmain from '../../../../src/assets/banner-main.png'
+import Bground from '../../../../src/assets/bg-shadow.png';
+
 const Hero = () => {
+
+    const customCss = {
+        backgroundImage: `url(${Bground})`,
+        backgroundSize: 'cover'
+    }
+
     return (
-        <div>
-            This is Banner section            
-        </div>
+        <main className='mx-3'>
+            <div
+                style={customCss}
+                className='py-16 px-2.5 rounded-3xl bg-[#1e1e1e] w-full'
+            >
+
+                <div className='mx-auto max-w-4xl flex flex-col justify-center'>
+
+                    <div className='mx-auto'>
+                        <img src={Bmain} alt="" srcset="" className='w-40 lg:w-auto'/>
+                    </div>
+                    
+                    
+                    <div className='mt-6'>
+                        <h2 className='font-bold text-white text-2xl md:text-4xl'>
+                        Assemble Your Ultimate Dream 11 Cricket Team
+                        </h2>
+
+                        <p className='mt-4 text-white opacity-70'>
+                        Beyond Boundaries Beyond Limits
+                        </p>
+
+                        <button className='btn'>Claim Free Credit</button>
+                    </div>
+
+                </div>
+
+            </div>            
+        </main>
     );
 };
 
--- src/Functions/Components/Header/Header.jsx
+++ src/Functions/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='backdrop-blur-lg py-6 sticky top-0'>
 
-            <div className='flex justify-between container mx-auto'>
+            <div className='flex justify-between container mx-auto px-2 md:px-3'>
 
                 {/* Logo */}
                 <div>
